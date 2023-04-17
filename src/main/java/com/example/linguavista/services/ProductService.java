9f7f9186d5706b7ae2eb81a4e5a252cad95a0f30
Albrut <115610709+Albrut@users.noreply.github.com>
2023-04-17 18:54:24 +0600

    Next lesson

--- src/main/java/com/example/linguavista/services/ProductService.java
+++ src/main/java/com/example/linguavista/services/ProductService.java
@@ -1,25 +1,39 @@
 package com.example.linguavista.services;
 
 import com.example.linguavista.models.Product;
+import com.example.linguavista.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RequiredArgsConstructor
+@Slf4j
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
     private List<Product> products = new ArrayList<>();
     private long ID = 0;
     {
         products.add(new Product(++ID,"Playstation 5", "console", 67000, "Krasnoyarsk", "tomas"));
         products.add(new Product(++ID,"Iphone 9", "phone", 50000, "DA", "AE"));
     }
-    public List<Product> listProducts(){return  products;}
+    public List<Product> listProducts(String title){
+        List<Product> products = productRepository.findAll();
+        if (title != null) productRepository.findByTitle(title);
+        return  productRepository.findAll();
+    }
     public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
     public void deleteProduct(Long id){
-       products.removeIf(product -> product.getId().equals(id));
+       productRepository.deleteById(id);
     }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
 }
